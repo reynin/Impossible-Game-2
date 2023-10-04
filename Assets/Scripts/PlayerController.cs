84b5307d5de9503162ba7ca1f8ca490d95dbafd5
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-04 13:16:58 +0200

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0,04f; //Field
+    public float speed = 0.04f; //Field
     // Start is called before the first frame update
     void Start()
     {
