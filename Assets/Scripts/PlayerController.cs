512a50625d3d4b5d83b0a73b3d9f59854a487789
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-09 10:43:33 +0200

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,38 +5,49 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.04f; //Field
-
-    private float jumpForce = 250;
+    public float jumpForce = 250;
+    public float jumpSpinForward = 2;
+    public float fallGravity = -1;
+    public int targetFrameRate;
     
-    // Start is called before the first frame update
+    private const float fallTolerance = -.1f;
+
     void Start()
     {
-        
+        Application.targetFrameRate = targetFrameRate;
     }
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
         Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>();
-        if (rigidBody.velocity.y < -.1f)
+        if (rigidBody.velocity.y < fallTolerance)
         {
-            rigidBody.AddForce(0, -1, 0);
+            rigidBody.AddForce(0, fallGravity, 0);
         }
+
+        rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, speed);
     }
+    // The Jump mechanic for player
+    void JumpForce()
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(0, jumpForce, 0);
+        rb.angularVelocity = new Vector3(jumpSpinForward, 0, 0);
+    }
+
+    bool IsTouchingGround()
+        {
+            int layerMask = LayerMask.GetMask("Ground");
+            return Physics.CheckBox(transform.position, transform.lossyScale / 1.99f, transform.rotation, layerMask);
+        }
+    
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Jump") && IsTouchingGround())
         {
-            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-            rb.AddForce(0,jumpForce, 0);
-            rb.angularVelocity = new Vector3(2,0,0);
+            JumpForce();
         }
         transform.Translate(0,0,speed, Space.World); //Moving forward
-    }
-
-    bool IsTouchingGround()
-    {
-        int layerMask = LayerMask.GetMask("Ground");
-        return Physics.CheckBox(transform.position, transform.lossyScale / 1.99f, transform.rotation, layerMask);
     }
 }
