3e8914857b5eb6bc35a46082fb21e87fba646ffb
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-10 09:53:01 +0200

    Update JumpPad.cs

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class JumpPad : MonoBehaviour
 {
     public float jumpForce = 20;
-    private void OnTriggerEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
         Rigidbody rb = other.gameObject.GetComponent <Rigidbody>();
         Vector3 velocity = rb.velocity;
